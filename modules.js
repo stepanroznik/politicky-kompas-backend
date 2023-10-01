ae6f455aa39bff90632dea0a34efe4f823d843d4
roznik-navisys <roznik@navisys.cz>
2023-10-01 22:53:11 +0200

    feat: temporarily removed swagger plugin

--- modules.js
+++
@@ -1,9 +0,0 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const path = require('path');
-
-const nodeModulePaths = [
-    (0, path.join)(process.cwd(), 'node_modules'),
-    ...module.paths,
-];
-
-console.log(nodeModulePaths);
